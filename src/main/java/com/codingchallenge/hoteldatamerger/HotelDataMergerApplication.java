9b68e04ff2c83240e28065dfa8c4b48b311e35f1
kusalkankanamge <17594647+kusaldushmantha@users.noreply.github.com>
2025-01-09 10:31:25 +0800

    Add initial boilerplate

---
+++ src/main/java/com/codingchallenge/hoteldatamerger/HotelDataMergerApplication.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.hoteldatamerger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HotelDataMergerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HotelDataMergerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingchallenge/hoteldatamerger/HotelDataMergerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingchallenge.hoteldatamerger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelDataMergerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
