0ad598bd21ee1c98e8998b8289f2c7296fa413b2
kusalkankanamge <17594647+kusaldushmantha@users.noreply.github.com>
2025-01-10 14:49:49 +0800

    Add controller

--- src/main/java/com/codingchallenge/hoteldatamerger/HotelController.java
+++ src/main/java/com/codingchallenge/hoteldatamerger/HotelController.java
@@ -1,11 +1,14 @@
 package com.codingchallenge.hoteldatamerger;
 
 import com.codingchallenge.hoteldatamerger.model.HotelResult;
+import com.codingchallenge.hoteldatamerger.sanitizer.Sanitize;
 import com.codingchallenge.hoteldatamerger.service.HotelService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashSet;
 import java.util.List;
 
 @RestController
@@ -19,7 +22,13 @@ public class HotelController {
     }
 
     @GetMapping
-    public List<HotelResult> getHotels() {
-        return this.hotelService.getAllHotels();
+    public List<HotelResult> getHotels(
+            @RequestParam(value = "destinationIDs", required = false) List<String> destinations,
+            @RequestParam(value = "hotelIDs", required = false) List<String> hotelIDs) {
+
+        List<String> sanitizedDestinationIDs = Sanitize.sanitizeStringList(destinations);
+        List<String> sanitizedHotelIDs = Sanitize.sanitizeStringList(hotelIDs);
+
+        return this.hotelService.getHotels(new HashSet<>(sanitizedDestinationIDs), new HashSet<>(sanitizedHotelIDs));
     }
 }
