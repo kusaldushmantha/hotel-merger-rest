235a11f1742fda8cfbfba61ba6602fc255ae5879
kusalkankanamge <17594647+kusaldushmantha@users.noreply.github.com>
2025-01-11 01:07:15 +0800

    Add pipeline

--- src/main/java/com/codingchallenge/hoteldatamerger/cachemanager/CacheManager.java
+++ src/main/java/com/codingchallenge/hoteldatamerger/cachemanager/CacheManager.java
@@ -4,9 +4,13 @@ import com.codingchallenge.hoteldatamerger.model.HotelResult;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 @Component
 public class CacheManager {
+    private static final Logger LOGGER = Logger.getLogger(CacheManager.class.getName());
+
     private final SimpleCache<String, List<HotelResult>> cache;
 
     public CacheManager(SimpleCache<String, List<HotelResult>> cache) {
@@ -17,8 +21,10 @@ public class CacheManager {
     public List<HotelResult> getFilteredResults(List<String> destinationIDs, List<String> hotelIDs) {
         String cacheKey = buildCacheKey(destinationIDs, hotelIDs);
         if (cache.get(cacheKey) != null) {
+            LOGGER.log(Level.INFO, new StringBuilder("Cache hit. Key: ").append(cacheKey).toString());
             return cache.get(cacheKey);
         }
+        LOGGER.log(Level.INFO, new StringBuilder("Cache miss. Key: ").append(cacheKey).toString());
         return null;
     }
 
@@ -26,6 +32,7 @@ public class CacheManager {
     public void addFilteredResult(List<String> destinationIDs, List<String> hotelIDs, List<HotelResult> result) {
         String cacheKey = buildCacheKey(destinationIDs, hotelIDs);
         this.cache.put(cacheKey, result);
+        LOGGER.log(Level.INFO, new StringBuilder("Added to cache. Key: ").append(cacheKey).toString());
     }
 
     // Build the cache key based on destinationIDs and hotelIDs
